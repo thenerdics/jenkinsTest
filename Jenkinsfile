182ffe36c98b5fcd245c08fee5d141abcb2d84ab
Manvir Brar <manvirbrar@Manvirs-MacBook-Pro.local>
2019-02-09 18:52:17 +0000

    test

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,11 +1,4 @@
-job('example') {
-    stage('notifications') {
-            notifications {
-                endpoint( 'http://localhost', 'TCP', 'XML') {
-                    event('started')
-                    timeout(60)
-                    logLines(100)
-                }
-            }
-    }
+node {
+   def thing = load 'job.groovy'
+   echo thing.doStuff()
 }
--- job.groovy
+++ job.groovy
@@ -1,13 +1,7 @@
 #!/usr/bin/env groovy
 
-job('example') {
-    stage('notifications') {
-            notifications {
-                endpoint( 'http://localhost', 'TCP', 'XML') {
-                    event('started')
-                    timeout(60)
-                    logLines(100)
-                }
-            }
-    }
+class job {
+   def doStuff() { return "HI" }
 }
+
+return new Thing();
