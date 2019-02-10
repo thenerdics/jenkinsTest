d57f74bb2c41d830f16c8606cfac03b6b46bcc14
Manvir Brar <manvirbrar@Manvirs-MacBook-Pro.local>
2019-02-10 12:20:44 +0000

    groovy test

--- variables/vars/job.groovy
+++ variables/vars/job.groovy
@@ -1,7 +1,31 @@
 #!/usr/bin/env groovy
 
-class job {
-   def doStuff() { return "HI" }
+class Student {
+   private int StudentID;
+   private String StudentName;
+	
+   void setStudentID(int pID) {
+      StudentID = pID;
+   }
+	
+   void setStudentName(String pName) {
+      StudentName = pName;
+   }
+	
+   int getStudentID() {
+      return this.StudentID;
+   }
+	
+   String getStudentName() {
+      return this.StudentName;
+   }
+	
+   static void main(String[] args) {
+      Student st = new Student();
+      st.setStudentID(1);
+      st.setStudentName("Joe");
+		
+      println(st.getStudentID());
+      println(st.getStudentName());
+   } 
 }
-
-return new Thing();
