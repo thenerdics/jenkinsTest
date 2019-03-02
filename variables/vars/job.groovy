896915677f45bb568ce071f20cff4f4fee63b778
Manvir Brar <manvirbrar@Manvirs-MacBook-Pro.local>
2019-03-02 13:48:27 +0000

    maven install

--- variables/vars/job.groovy
+++ variables/vars/job.groovy
@@ -1,2 +1,12 @@
 #!/usr/bin/env groovy
 
+pipeline {
+    agent { docker 'maven:3-alpine' } 
+    stages {
+        stage('Example Build') {
+            steps {
+                sh 'mvn -B clean verify'
+            }
+        }
+    }
+}
