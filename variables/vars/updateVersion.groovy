367c49bd5de316ce36614bb88614ba81e7446058
Manvir Brar <manvirbrar@Manvirs-MacBook-Pro.local>
2019-03-03 13:57:43 +0000

    variables

--- variables/vars/updateVersion.groovy
+++ variables/vars/updateVersion.groovy
@@ -2,6 +2,7 @@
 
 def increment(){
     sh '''
+    echo "Incrementing"
         git checkout mavenTest
         mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.nextMinorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit
     ''' 
