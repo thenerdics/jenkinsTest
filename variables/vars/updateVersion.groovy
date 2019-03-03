8b1526a84aea1ca13ef66fe2d0c3be633d8e133e
Manvir Brar <manvirbrar@Manvirs-MacBook-Pro.local>
2019-03-03 23:45:15 +0000

    hello world test

--- variables/vars/updateVersion.groovy
+++ variables/vars/updateVersion.groovy
@@ -15,7 +15,7 @@ def mavenIncrement(choice){
             mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.nextIncrementalVersion} versions:commit
         '''
     } else if (choice == 'hotfix'){
-        sh """
+        sh"""
         message=${choice}
         echo "$message"
             mvn build-helper:parse-version versions:set -DnewVersion=\\${parsedVersion.majorVersion}.\\${parsedVersion.minorVersion}.\\${parsedVersion.incrementalVersion}."$message" versions:commit
